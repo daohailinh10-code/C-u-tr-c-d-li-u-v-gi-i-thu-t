bab69e62265c8e407fe1d1540547e8e131c5b9da
daohailinh10-code <daohailinh10@gmail.com>
2026-06-29 23:18:06 +0700

    Refactor Dijkstra and Prim algorithms with new variables

--- Thuat_toan_Dijkstra_PRIM.cpp
+++ Thuat_toan_Dijkstra_PRIM.cpp
@@ -8,22 +8,22 @@ using namespace std;
 const int INF = 1e9;
 const int MAX = 100;
 
-int G[MAX][MAX]; // Ma trận kề đồ thị gốc
+int G[MAX][MAX]; 
 int n; // Số đỉnh
 
 // 1. Thuật toán Dijkstra
 void Dijkstra(int start) {
     vector<int> dist(n, INF);
-    vector<bool> visited(n, false);
+    vector<bool> to(n, false);
     dist[start] = 0;
 
     for (int i = 0; i < n; i++) {
         int u = -1;
         for (int j = 0; j < n; j++) 
-            if (!visited[j] && (u == -1 || dist[j] < dist[u])) u = j;
+            if (!to[j] && (u == -1 || dist[j] < dist[u])) u = j;
         
         if (dist[u] == INF) break;
-        visited[u] = true;
+        to[u] = true;
         for (int v = 0; v < n; v++) 
             if (G[u][v] && dist[u] + G[u][v] < dist[v]) dist[v] = dist[u] + G[u][v];
     }
@@ -33,10 +33,10 @@ void Dijkstra(int start) {
 }
 
 void Prim() {
-    int parent[MAX], key[MAX];
+    int dinh[MAX], key[MAX];
     bool mstSet[MAX];
     for (int i = 0; i < n; i++) key[i] = INF, mstSet[i] = false;
-    key[0] = 0; parent[0] = -1;
+    key[0] = 0; dinh[0] = -1;
 
     for (int count = 0; count < n - 1; count++) {
         int u = -1;
@@ -44,13 +44,12 @@ void Prim() {
             if (!mstSet[v] && (u == -1 || key[v] < key[u])) u = v;
         mstSet[u] = true;
         for (int v = 0; v < n; v++)
-            if (G[u][v] && !mstSet[v] && G[u][v] < key[v]) parent[v] = u, key[v] = G[u][v];
+            if (G[u][v] && !mstSet[v] && G[u][v] < key[v]) dinh[v] = u, key[v] = G[u][v];
     }
 
-    // In ma trận đỉnh kề của cây khung
     int MST[MAX][MAX] = {0};
     for (int i = 1; i < n; i++) {
-        MST[i][parent[i]] = MST[parent[i]][i] = G[i][parent[i]];
+        MST[i][dinh[i]] = MST[dinh[i]][i] = G[i][dinh[i]];
     }
     cout << "Ma tran ke cay khung (Prim):" << endl;
     for (int i = 0; i < n; i++) {
@@ -59,12 +58,11 @@ void Prim() {
     }
 }
 
-// 3. Cấu trúc cho Kruskal
 struct Edge { int u, v, w; };
 bool compareEdges(Edge a, Edge b) { return a.w < b.w; }
 
 int find(int i, vector<int>& parent) {
-    return (parent[i] == i) ? i : (parent[i] = find(parent[i], parent));
+    return (dinh[i] == i) ? i : (đinh[i] = find(dinh[i], dinh));
 }
 
 void Kruskal() {
@@ -79,10 +77,10 @@ void Kruskal() {
 
     int MST[MAX][MAX] = {0};
     for (auto& e : edges) {
-        int rootU = find(e.u, parent), rootV = find(e.v, parent);
+        int rootU = find(e.u, dinh), rootV = find(e.v, dinh);
         if (rootU != rootV) {
             MST[e.u][e.v] = MST[e.v][e.u] = e.w;
-            parent[rootU] = rootV;
+            dinh[rootU] = rootV;
         }
     }
     cout << "Ma tran ke cay khung (Kruskal):" << endl;
